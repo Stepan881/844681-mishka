6d2a11d037e8ff0aeb5c24eed278b20cb2a086cc
Stepan <wwwstepanwww@mail.ru>
2018-10-14 18:14:38 +0300

    JS

---
+++ source/js/popup.js
@@ -0,0 +1,16 @@
+var popap  = document.querySelector(".popap");
+var popapopen = document.querySelector(".baskets__btn-order");
+var popapcloseesc = document.querySelector(".popap__overoverlay");
+
+popapopen.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popap.classList.remove("popap__checked");
+  console.log("open");
+});
+
+
+popapcloseesc.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popap.classList.add("popap__checked");
+  console.log("close");
+});
---
+++ source/js/popupa.js
@@ -0,0 +1,9 @@
+var catawdwdw = document.querySelector(".catalog__icon");
+  console.log("q111");
+
+
+catawdwdw.addEventListener("click", function(evt){
+  console.log("opssssen");
+  evt.preventDefault();
+  console.log("open");
+});
