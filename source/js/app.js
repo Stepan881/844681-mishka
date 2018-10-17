d50015569be646b09876bf8571a0ac38c60cc6c7
Stepan <wwwstepanwww@mail.ru>
2018-10-17 22:18:15 +0300

    Index

--- source/js/app.js
+++ source/js/app.js
@@ -1,15 +1,14 @@
-var popap  = document.querySelector(".popap");
+var popap = document.querySelector(".popap");
 var popapopen = document.querySelector(".baskets__btn-order");
 var popapcloseesc = document.querySelector(".popap__overoverlay");
 
-popapopen.addEventListener("click", function(evt){
+popapopen.addEventListener("click", function(evt) {
   evt.preventDefault();
   popap.classList.remove("popap__checked");
   console.log("open");
 });
 
-
-popapcloseesc.addEventListener("click", function(evt){
+popapcloseesc.addEventListener("click", function(evt ){
   evt.preventDefault();
   popap.classList.add("popap__checked");
   console.log("close");
