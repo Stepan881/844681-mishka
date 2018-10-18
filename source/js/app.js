01dbc882b14227dcc10e09a93a239f86b0f58e79
Stepan <wwwstepanwww@mail.ru>
2018-10-19 00:37:17 +0300

    Доделки

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,23 @@
 var popap = document.querySelector(".popap");
 var popapopen = document.querySelector(".baskets__btn-order");
 var popapcloseesc = document.querySelector(".popap__overoverlay");
+var myList = document.querySelectorAll(".catalog__icon");
+
+console.log(myList);
+
+for (var i = 0; i < myList.length; i++) {
+  myList[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    console.log('press');
+    popap.classList.remove("popap__checked");
+  });
+
+  popapcloseesc.addEventListener("click", function(evt){
+    evt.preventDefault();
+    popap.classList.add("popap__checked");
+    console.log("close");
+  });
+}
 
 popapopen.addEventListener("click", function(evt) {
   evt.preventDefault();
@@ -8,7 +25,7 @@ popapopen.addEventListener("click", function(evt) {
   console.log("open");
 });
 
-popapcloseesc.addEventListener("click", function(evt ){
+popapcloseesc.addEventListener("click", function(evt){
   evt.preventDefault();
   popap.classList.add("popap__checked");
   console.log("close");
