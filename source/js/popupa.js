f1bc6f0445cc3844650ad203731f0327f7472247
Stepan <wwwstepanwww@mail.ru>
2018-10-19 13:53:16 +0300

    финал

--- source/js/popupa.js
+++
@@ -1,9 +0,0 @@
-var catawdwdw = document.querySelector(".catalog__icon");
-  console.log("q111");
-
-
-catawdwdw.addEventListener("click", function(evt){
-  console.log("opssssen");
-  evt.preventDefault();
-  console.log("open");
-});
